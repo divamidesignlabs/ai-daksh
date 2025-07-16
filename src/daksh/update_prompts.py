bf6e66e5f7cd1d60730718a58036f717cca19c04
Yeshwanth <1992chinna@gmail.com>
2025-07-16 10:58:04 +0530

    daksh can update the latest prompts in any repo

---
+++ src/daksh/update_prompts.py
@@ -0,0 +1,20 @@
+import shutil
+from torch_snippets import *
+
+from .__pre_init__ import cli
+
+
+@cli.command()
+def update_prompts(dry_run: bool = True):
+    prompts_folder: P = current_file_dir(__file__) / "prompt-templates"
+    for f in ls(prompts_folder):
+        to = f"./{f.name}"
+        Info(f"Updating {f} to {to}")
+
+        if dry_run:
+            continue
+
+        if f.is_file():
+            f.cp(to)
+        elif f.is_dir():
+            shutil.copytree(f, to, dirs_exist_ok=True)
