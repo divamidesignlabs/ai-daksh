cd8df80b169e461809505cfa1959accee3cdae5d
Yeshwanth <1992chinna@gmail.com>
2025-07-27 16:04:45 +0530

    remove docs

--- src/daksh/__init__.py
+++ src/daksh/__init__.py
@@ -1,18 +1,14 @@
 """Top-level package for DAKSH."""
 
-__all__ = [
-    '__version__',
-    '__author__',
-    '__email__',
-    'cli',
-    'health',
-    'io'
-]
+__all__ = ["__version__", "__author__", "__email__", "cli", "health", "io"]
 
 __author__ = """Yeshwanth Reddy"""
-__email__ = 'yeshwanth@divami.com'
-__version__ = '0.1.0'
+__email__ = "yeshwanth@divami.com"
+__version__ = "0.1.0"
 
 from .__pre_init__ import cli
 from .health import *
 from .update_prompts import *
+from .jira import *
+
+cli.add_typer(jira_cli, name="jira", help="Jira CLI interface")
